f213c31fc2c96811b30674db2f8b470dfde31fbf
acastelb <acastelb@student.42.fr>
2021-06-14 13:58:31 +0200

    update mysql entry

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,5 +1,6 @@
 mysql_install_db
 mysqld_safe --datadir='/var/lib/mysql'
-msql -e "CREATE USER '$USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASS';"
+echo "ooooo"
+mysql -e "CREATE USER '$USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASS';"
 mysql -e "GRANT ALL PRIVILEGES ON * . * TO '$USER_NAME'@'localhost';"
 mysql -e "FLUSH PRIVILEGES;"
