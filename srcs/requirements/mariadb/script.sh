f800f3e3b87d1611bd488f8568cf10972a834666
acastelb <acastelb@student.42.fr>
2021-06-14 14:06:46 +0200

    mysql arent in background

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,6 +1,5 @@
 mysql_install_db
 mysqld_safe --datadir='/var/lib/mysql'
-echo "ooooo"
 mysql -e "CREATE USER '$USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASS';"
 mysql -e "GRANT ALL PRIVILEGES ON * . * TO '$USER_NAME'@'localhost';"
 mysql -e "FLUSH PRIVILEGES;"
