f9ee33b6537826c0a59d8f8438fb7c23f480c5e2
acastelb <acastelb@student.42.fr>
2021-06-14 11:09:54 +0200

    add my.cnf volumes ok, try to confing my.cnf

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,5 +1,5 @@
-service mysql start
+mysql_install_db
+mysqld
 mysql -e "CREATE USER '$USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASS';"
 mysql -e "GRANT ALL PRIVILEGES ON * . * TO '$USER_NAME'@'localhost';"
 mysql -e "FLUSH PRIVILEGES;"
-mysqld
