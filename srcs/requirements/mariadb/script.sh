ae914140946f299fa0739a3ac1faf3c89ebf646c
acastelb <acastelb@student.42.fr>
2021-06-15 14:59:55 +0200

    mysql work but use of service in script is forbiddent

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,5 +1,7 @@
-mysql_install_db
-mysqld_safe --datadir='/var/lib/mysql'
+service mysql start
 mysql -e "CREATE USER '$USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASS';"
 mysql -e "GRANT ALL PRIVILEGES ON * . * TO '$USER_NAME'@'localhost';"
 mysql -e "FLUSH PRIVILEGES;"
+service mysql stop
+mysql_install_db
+mysqld_safe --datadir='/var/lib/mysql'
