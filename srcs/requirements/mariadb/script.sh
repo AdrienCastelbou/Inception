927ddf3617bdff166ca6f107bbca54e5fc6e48ac
acastelb <acastelb@student.42.fr>
2021-06-15 15:18:28 +0200

    set db

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,5 +1,6 @@
 service mysql start
-mysql -e "CREATE USER '$USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASS';"
+mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
+mysql -e "CREATE USER IF NOT EXISTS '$USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASS';"
 mysql -e "GRANT ALL PRIVILEGES ON * . * TO '$USER_NAME'@'localhost';"
 mysql -e "FLUSH PRIVILEGES;"
 service mysql stop
