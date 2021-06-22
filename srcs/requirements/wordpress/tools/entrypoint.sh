a865dbfe4a1d8001beda131317f3e99239ddd8cc
acastelb <acastelb@student.42.fr>
2021-06-22 10:51:50 +0200

    add if in wp entrypt

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,3 +1,6 @@
-mv wp-config.php /wordpress
-mv wordpress /var/www/html
+if ! [ -d "/var/www/html/wordpress" ]; 
+then
+	mv wp-config.php /wordpress
+	mv wordpress /var/www/html
+fi
 php-fpm7.3 -R -F
