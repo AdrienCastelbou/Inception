1a04876b42e0421129f8ae5c93ec250af920257a
acastelb <acastelb@student.42.fr>
2021-06-24 16:10:15 +0200

    wordpress load but error with db connection

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -21,10 +21,10 @@
  */
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
-define('DB_NAME', get_env('WORDPRESS_DB_NAME'));
-define('DB_USER', get_env('WORDPRESS_DB_USER'));
-define('DB_PASSWORD', get_env('WORDPRESS_DB_PASSWORD'));
-define('DB_HOST', 'WORDPRESS_DB_HOST');
+define('DB_NAME', getenv('WORDPRESS_DB_NAME'));
+define('DB_USER', getenv('WORDPRESS_DB_USER'));
+define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
+define('DB_HOST', getenv('WORDPRESS_DB_HOST'));
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
