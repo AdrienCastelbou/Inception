d58057319e506661ab6ce3f523718749503938c9
acastelb <acastelb@student.42.fr>
2021-06-25 16:06:14 +0200

    connection wp-mysql ok but script dont create db, user

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,8 +1,8 @@
 mysql_install_db
 mysqld &
 mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE"
-mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'$WORDPRESS_DB_HOST' IDENTIFIED BY '$MYSQL_USER';"
-mysql -e "GRANT ALL PRIVILEGES ON * . * TO '$MYSQL_USER'@'$WORDPRESS_DB_HOST';"
+mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -e "GRANT ALL PRIVILEGES ON * . * TO '$MYSQL_USER'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
 mysql <<EOF
 USE $MYSQL_DATABASE;
@@ -11,8 +11,6 @@ CREATE TABLE IF NOT EXISTS $MYSQL_DATABASE.members(
 		name varchar(255) not null,
 		primary key(member_id)
 	);
-	INSERT INTO members (name) VALUES ('Thomas (Neo)');
-	INSERT INTO members (name) VALUES ('BIGGIE');
 EOF
 pkill mysqld
 mysqld_safe --datadir='/var/lib/mysql'
