57e10508aa60256bfbe63e3e6d0591e7a240524f
acastelb <acastelb@student.42.fr>
2021-06-30 11:15:58 +0200

    wp install via wp-cli ok

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,10 +1,9 @@
 if ! [ -d "/var/www/html/wordpress" ]; 
 then
-	sed -i "s/WP_DB_NAME/'$WP_DB_NAME'/g" /wp-config.php
-	sed -i "s/WP_DB_HOST/'$WP_DB_HOST'/g" /wp-config.php
-	sed -i "s/WP_DB_USER/'$WP_DB_USER'/g" /wp-config.php
-	sed -i "s/WP_DB_PASSWORD/'$WP_DB_PASSWORD'/g" /wp-config.php
-	mv wp-config.php /wordpress
+	sleep 5
 	mv wordpress /var/www/html
+	wp config create  --allow-root --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASSWORD --path=/var/www/html/wordpress --dbhost=mariadb
+	sleep 5
+	wp core install --url=acastelb.42.fr --title=Inception --admin_user="lol" --admin_password="lol_pass" --admin_email="lol@gmail.com" --allow-root --path=/var/www/html/wordpress 
 fi
 exec php-fpm7.3 -R -F
