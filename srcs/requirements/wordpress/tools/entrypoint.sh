23dbf7ce4f33577d4ef36bb9061a972039e10722
acastelb <acastelb@student.42.fr>
2021-07-02 15:32:17 +0200

    use named volume

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,9 +1,9 @@
 if ! [ -d "/var/www/html/wordpress" ]; 
 then
-	sleep 5
+	sleep 2
 	mv wordpress /var/www/html
 	wp config create  --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --path=/var/www/html/wordpress --dbhost=$WP_DB_HOST
-	sleep 5
+	sleep 2
 	wp core install --url=$DOMAIN_NAME --title=Inception --admin_user=$WPADMIN_NAME --admin_password=$WPADMIN_PASSWORD --admin_email="$WPADMIN_NAME@gstudent.42.fr" --allow-root --path=/var/www/html/wordpress
 	wp user create $WPUSER_NAME "$WPUSER_NAME@student.42.fr" --role=author --user_pass=$WPUSER_PASSWORD --allow-root --path=/var/www/html/wordpress
 fi
